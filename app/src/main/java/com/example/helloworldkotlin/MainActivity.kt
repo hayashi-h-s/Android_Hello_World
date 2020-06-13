0b2dead706e8cbc966a79f49d500879d978ae524
Haya <max9173627@gmail.com>
2020-06-13 10:18:37 +0900

    buttonクリック時にtextを変える

--- app/src/main/java/com/example/helloworldkotlin/MainActivity.kt
+++ app/src/main/java/com/example/helloworldkotlin/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.helloworldkotlin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+//import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        textHelloWorld.text = "CHANGE!!"
+
+        var hello : TextView = findViewById(R.id.textView)
+        var hellobutton : Button = findViewById(R.id.button2)
+        hellobutton.setOnClickListener {
+            hello.text = "Change"
+        }
     }
 }
